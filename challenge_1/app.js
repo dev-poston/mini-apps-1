6594405c5d4bd76b711cbbe07a0d7f731b34e737
Devon Poston <dev.poston@gmail.com>
2021-03-23 15:36:30 -0700

    created board obj, switch player function, html board table, working on handleclick function, added event listener to all cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,30 @@
-console.log('hello david');
+const playerX = 'X';
+const playerO = 'O';
+const cells = document.getElementsByClassName('cell');
+let swap = false;
+
+let board = {
+  row1: [0, 0, 0],
+  row2: [0, 0, 0],
+  row3: [0, 0, 0]
+};
+
+let switchPlayer = () => {
+  swap = !swap;
+};
+
+let handleClick = (e) => {
+  console.log('click');
+  if (!swap) {
+    e.target.textContent = playerX;
+  } else {
+    e.target.textContent = playerO;
+  }
+  switchPlayer();
+};
+
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', handleClick, {once: true});
+};
+
+console.log('Playing Tic Tac Toe @', new Date());
