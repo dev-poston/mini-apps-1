219f6bc5e73817080e60a794d66799786d70b84f
Devon Poston <dev.poston@gmail.com>
2021-03-26 22:57:01 -0700

    finally got uploaded json file content coming thru on post request

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+var multer  = require('multer')
+var upload = multer();
 
 app.use(express.static('views'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); //{extended: false} or true???
+app.use(cors());
 app.set('views', './views');
 app.set('view engine', 'ejs')
 
@@ -12,44 +19,64 @@ app.get('/', (req, res) => {
   res.render('index', {reports: null})
 });
 
-app.post('/', (req, res) => {
-  console.log('POST REQ MADE');
-  let parse = JSON.parse(req.body.text);
+app.post('/', upload.any(), (req, res) => {
+  console.log('POST REQ MADE', req.files);
 
-  let parseBody = (req) => {
-    let csvReport = '';
-    for (let key in req) {
-      if (key !== 'children') {
-        csvReport += (key + ',');
-      }
-    }
-    csvReport = csvReport.slice(0, -1) + '\n';
-    let bodySearch = (body) => {
-      if (!body.children) {
-        return;
-      }
-      for (let val in body) {
-        if (val !== 'children') {
-          csvReport += (body[val] + ',');
-        } else {
-          if (val) {
-            for (let i = 0; i < body[val].length; i++) {
-              csvReport = csvReport.slice(0, -1) + '\n';
-              bodySearch(body[val][i])
-            }
-          }
-        }
-      }
-      csvReport = csvReport.slice(0, -1) + '\n';
-    }
-    bodySearch(req);
-    return csvReport;
-  }
+  let buf = req.files[0].buffer.toString();
+  // console.log(buf.toString());
+  // let parse = JSON.parse(req.body);
 
-  let report = parseBody(parse);
-  console.log(report);
-  res.status(200);
-  res.render('index', {reports: report});
+  // let parseBody = (req) => {
+  //   let csvReport = '';
+  //   for (let key in req) {
+  //     if (key !== 'children') {
+  //       csvReport += (key + ',');
+  //     }
+  //   }
+  //   csvReport = csvReport.slice(0, -1) + '\n';
+  //   let bodySearch = (body) => {
+  //     if (!body.children) {
+  //       return;
+  //     }
+  //     for (let val in body) {
+  //       if (val !== 'children') {
+  //         csvReport += (body[val] + ',');
+  //       } else {
+  //         if (val) {
+  //           for (let i = 0; i < body[val].length; i++) {
+  //             csvReport = csvReport.slice(0, -1) + '\n';
+  //             bodySearch(body[val][i])
+  //           }
+  //         }
+  //       }
+  //     }
+  //     csvReport = csvReport.slice(0, -1) + '\n';
+  //   }
+  //   bodySearch(req);
+  //   return csvReport;
+  // }
+  // let report = parseBody(req.body);
+
+  fs.writeFile(`${req.files[0].originalname}`, `${buf}`, 'utf8', (err) => {
+    if (err) {
+        console.log('Error!');
+        throw err;
+    } else {
+        console.log('Saved!');
+      }
+    });
+    // fs.readFile(path.join(__dirname, `${req.body.file}`), 'utf8', (err, data) => {
+    //     if ( err ) {
+    //         console.log( 'error', err );
+    //     } else {
+    //         console.log('file read');
+    //         console.log(data);
+    //     }
+    // });
+
+  // console.log(report);
+  // res.status(200);
+  // res.render('index', {reports: report});
 });
 
 app.listen(3000, () => {
